5c98dc1d18ecd963691580c770b81b2e4fd970a7
akhilrana0001 <akhilrana0001@gmail.com>
2020-02-22 04:00:03 +0530

    square-option-init

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const canvas = document.getElementById("shape");
 const ctx = canvas.getContext("2d");
 
-$(document).ready(function() {
-  $("#drawRect").click(function() {
+$("#drawFig").click(function() {
+  let selectedValue = $(".shapeSelect").val();
+  if (selectedValue == 2) {
     let rectWidth = $("#RectWidth").val();
     let rectHeight = $("#RectHeight").val();
     let xoff = $("#xoff").val();
@@ -19,5 +20,32 @@ $(document).ready(function() {
       ctx.strokeStyle = color;
       ctx.strokeRect(xoff, yoff, rectWidth, rectHeight);
     }
-  });
+  } else if (selectedValue == 1) {
+    let side = $("#side").val();
+    let xoffs = $("#xoffs").val();
+    let yoffs = $("#yoffs").val();
+    let colors = "#" + $("#colorSquare").val();
+
+    if ($(".clearCanvas").is(":checked")) {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+    if ($(".colorFill").is(":checked")) {
+      ctx.fillStyle = colors;
+      ctx.fillRect(xoffs, yoffs, side, side);
+    } else {
+      ctx.strokeStyle = color;
+      ctx.strokeRect(xoffs, yoffs, side, side);
+    }
+  }
+});
+
+$(".shapeSelect").change(function() {
+  let selectedValue = $(this).val();
+  if (selectedValue == 1) {
+    $(".RectInput").css("display", "none");
+    $(".SquareInput").css("display", "block");
+  } else if (selectedValue == 2) {
+    $(".SquareInput").css("display", "none");
+    $(".RectInput").css("display", "block");
+  }
 });
